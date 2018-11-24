cdca0440b6eac491ca22cf0d1c63572f13e8a131
rvertwo <44541187+rvertwo@users.noreply.github.com>
2018-11-24 00:31:24 -0800

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Amazon's Best Prices")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Able to track 5 items on Amazon.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Amazon's Best Prices")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © rvertwo 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
